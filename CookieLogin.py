e255d7b73c7e49e31aa7c67e389949dd33926a58
liujianfei <liujianfei@ict.ac.cn>
2016-02-05 22:09:06 +0800

    spider

---
+++ CookieLogin.py
@@ -0,0 +1,73 @@
+__author__ = 'liujianfei'
+# -*- coding:utf-8 -*-
+import urllib
+import urllib2
+import cookielib
+import re
+import random
+import requests
+import string
+
+class CookieLogin:
+    #页面初始化
+    def __init__(self):
+        self.fileName = 'cookie.txt'
+        self.siteURL = 'https://passport.csdn.net/'
+        #声明一个MozillaCookieJar对象实例来保存cookie，之后写入文件
+        self.cookie = cookielib.MozillaCookieJar(self.fileName)
+        #利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
+        self.handler= urllib2.HTTPCookieProcessor(self.cookie)
+        
+        self.postdata = urllib.urlencode({
+                    'userName':'liujianfei526@163.com',
+                    'pwd':'919526'
+                })
+        self.loginHeaders =  {
+            'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0',
+            'Referer': 'ttp://passport.csdn.net/account/login',
+            'Connection' : 'keep-alive'
+        } 
+        self.pageNum = 0
+        # use proxy ip 
+        #self.ips_list = ['222.186.12.180:55336']
+        #print '使用的代理ip地址： ' + self.ip
+        #self.proxy_support = urllib2.ProxyHandler({'http':'http://'+random.choice(self.ips_list)})
+        self.opener = urllib2.build_opener(self.handler)
+        #通过handler来构建opener
+        #self.opener = urllib2.build_opener(self.handler)
+        urllib2.install_opener(self.opener)
+    def getLoginInfo(self):
+        #loginPage = self.opener.open(self.siteURL).text
+        loginPage = self.opener.open(self.siteURL).read()
+        pattern = re.compile('<input type="hidden".*?value="(.*?)" />',re.S)
+        result = re.findall(pattern,loginPage)
+        lt = result[0]
+        execution = result[1]
+        #print lt,execution
+        self.postdata = urllib.urlencode({
+                    "lt": lt,
+                    "execution": execution,
+                    "_eventId": "submit",
+                    'username':'liujianfei526@163.com',
+                    'password':'407526'
+                })
+        #print self.postdata
+    def login(self):
+        #print self.postdata
+        #模拟登录，并把cookie保存到变量
+        request = urllib2.Request(self.siteURL,self.postdata,self.loginHeaders) 
+        try:
+            response = self.opener.open(request)
+        except urllib.error.URLError as e:
+            print(e.reason)
+        #保存cookie到cookie.txt中
+        #print result
+        self.cookie.save(ignore_discard=True, ignore_expires=True) 
+        print "ok"
+#if __name__ == '__main__': 	
+    #login = CookieLogin()
+    #login.getLoginInfo()
+    #login.login()
+    #login.getPageNum("http://write.blog.csdn.net/postlist")
+    #print result
---
+++ signleThreadSpiderCSDN.py
@@ -0,0 +1,164 @@
+__author__ = 'liujianfei'
+# -*- coding:utf-8 -*-
+
+import urllib
+import urllib2
+import re
+import removeUnsedTagTool
+import os
+import CookieLogin
+import time
+#抓取MM
+class SpiderCSDN:
+    #页面初始化
+    def __init__(self):
+        self.fileName = ''
+        self.siteURL = ''
+        self.tool = ''
+        self.login = ''
+        self.opener = ''
+        self.postdata = ''
+        self.loginHeaders = ''
+        self.used = []
+        self.unused = []
+        self.hash = {}
+    def setLoginInfo(self,fileName,siteURL,login,opener,tool,postdata,loginHeaders):
+        '''设置用户登录信息'''
+        self.fileName = fileName
+        self.siteURL = siteURL
+        self.login = login
+        self.opener = opener
+        self.tool = tool
+        self.postdata = postdata
+        self.loginHeaders = loginHeaders
+    #处理异常
+    def openAndDoExcept(self,url):
+        request = urllib2.Request(url,self.postdata,self.loginHeaders)
+        try:
+            page = self.opener.open(request).read()
+            self.saveList(self.unused,"unused")
+        except urllib2.URLError, e:
+            self.saveList(self.unused,"unused")
+            self.saveList(self.used,"used")
+            self.saveList(self.hash,"hash")
+            if hasattr(e,"reason"):
+                print u"连接糗事百科失败,错误原因",e.reason
+                return None
+        return page
+    #获得相应页码文章
+    def getPageNum(self,url):
+        #利用cookie请求访问另一个网址
+        page = self.openAndDoExcept(url)
+        pattern = re.compile('<div class="page_nav"><span>.*?([\d]+).*?([\d]+).*?</span>',re.S)
+        result = re.findall(pattern,page)
+        #print page
+        if result:
+            #print "找到了共多少页"
+            self.pageNum = result[0][1]
+            #print '共',self.pageNum,'页'
+            return self.pageNum
+    #获得指定页码所有文章列表
+    def getPageList(self,index):
+        url = 'http://write.blog.csdn.net/postlist/0/0/enabled/'+str(index)
+        page = self.openAndDoExcept(url)
+        #<a href='' target=_blank></a>
+        #<td class='tdleft'><a href='' target=_blank></a><span class='gray'>
+        #获取相应的文章列表
+        pattern = re.compile("<td class='tdleft'><a href='(.*?)'.*?target=_blank>(.*?)</a><span class='gray'>",re.S)
+        result = re.findall(pattern,page)
+        #print page
+        for i in result:
+            self.unused.append(i)
+            self.pageNum = i[0]
+            #print i[0],i[1]
+            #return self.pageNum
+    #获得所有页码所有文章列表
+    def getAllPageList(self,PageNum):
+        #利用cookie请求访问
+        for i in range(1,PageNum):
+            #print "第"+str(i)+"页"
+            self.getPageList(i)
+        return self.unused
+    #获得单篇文章内容并写入文档并保存
+    def getPageContent(self,url):
+        #利用cookie请求访问
+        page = self.openAndDoExcept(url)
+        listALl = url.split('/')
+        name = listALl.pop()
+        pattern = re.compile("<p>(.*?)</p>",re.S)
+        result = re.findall(pattern,page)
+        #print result
+        contentList = []
+        for i in result:
+            i = self.tool.replace(i)
+            #print i
+            #contentList.append(i)
+            self.saveContent(i,name)
+    #获得列表中所有文章内容并写入文档并保存
+    def getPageAllContent(self):
+        #利用cookie请求访问
+        for i in self.unused:
+            self.used.append(i);
+            if self.hash.get(i[0])!=None:
+                continue
+            self.hash[i[0]]=i[1]
+            self.getPageContent(i[0])
+    #创建新目录
+    def mkdir(self,path):
+        path = path.strip()
+        # 判断路径是否存在
+        # 存在     True
+        # 不存在   False
+        isExists=os.path.exists(path)
+        # 判断结果
+        if not isExists:
+            # 如果不存在则创建目录
+            #print u"偷偷新建了名字叫做",path,u'的文件夹'
+            # 创建目录操作函数
+            os.makedirs(path)
+            return True
+        else:
+            # 如果目录存在则不创建，并提示目录已存在
+            #print u"名为",path,'的文件夹已经创建成功'
+            return False
+     #保存文章内容
+    def saveList(self,content,name):
+        fileName = "singleThread" + '/' + name + ".txt"
+        f = open(fileName,"w+")
+        #print u"正在偷偷保存她的个人信息为",fileName
+        for i in content:
+            if len(i)>1:
+                f.write(i[0]+'\n')
+                #print i[0]
+        f.close()
+     #保存文章内容
+    def saveContent(self,content,name):
+        fileName = "singleThread" + '/' + name + ".txt"
+        f = open(fileName,"a+")
+        #print u"正在偷偷保存她的个人信息为",fileName
+        f.write(content+'\n')
+        f.close()
+if __name__ == '__main__': 
+    start_time = time.ctime()
+    print "开始时间"+start_time	
+    fileName = "singleThread"
+    siteURL = "http://write.blog.csdn.net/postlist"
+    login = CookieLogin.CookieLogin()
+    login.getLoginInfo()
+    login.login()
+    opener = login.opener
+    tool = removeUnsedTagTool.removeUnsedTagTool()
+    Csdn = SpiderCSDN();
+    #初始化基本信息
+    Csdn.setLoginInfo(fileName,siteURL,login,opener,tool,login.postdata,login.loginHeaders)
+    #print 'ok'
+    #获取总页数
+    PageNum = int(Csdn.getPageNum(siteURL))+1
+    #print PageNum
+    Csdn.getAllPageList(PageNum)
+    Csdn.mkdir(fileName)
+    Csdn.getPageAllContent()
+    end_time = time.ctime()
+    print "结束时间"+end_time
+    #Csdn.getPageContent('http://blog.csdn.net/liujianfei526/article/details/50597031')
+    #print result
